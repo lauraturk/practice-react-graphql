24e0e71fd679d90d6e31eb651ee0afd9d9c9fb64
Laura Turk <laura.turk@cognizant.com>
2018-12-06 10:14:20 -0700

    complete video 8, router and nprogress

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,6 +1,18 @@
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import Nav from './Nav';
 import Link from 'next/link';
-import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
+Router.onRouteChangeStart = () => {
+    NProgress.start()
+}
+Router.onRouteChangeComplete = () => {
+    NProgress.done()
+}
+Router.onRouteChangeError = () => {
+    NProgress.done()
+}
 
 const Logo =styled.h1`
     font-size: 4rem;
