c0015e4bf5bf177e83f67747e1206f1a4413bbae
Laura Turk <laura.turk@cognizant.com>
2018-12-13 11:31:21 -0700

    complete vid 4, mutations and query connect w/ prisma

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,16 @@
-const Mutation = {};
+const Mutation = {
+   async createItem(parent, args, ctx, info) {
+    //TODO: checked if user is logged in
+
+    //createItem returns a promise
+    const item = await ctx.db.mutation.createItem({
+        data: {
+         ...args
+        }
+        // inside info is the actual query, mutation needs to take from front to backend, will specify what information is returned
+    }, info);
+    return item;
+   }
+};
 
 module.exports = Mutation;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,11 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+const Query = {
+    // can forward directly to prisma
+    // useful if you don't need to do anything special in this step
+    items: forwardTo('db'),
+//    async items(parent, args, ctx, info) {
+//        const items = await ctx.db.query.items();
+//    }
+};
 
 module.exports = Query;
